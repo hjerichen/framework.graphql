67250edc0b9899717cd69865bf591703441348e3
Heiko Jerichen <HeikoJer@gmx.de>
2023-05-26 23:36:29 +0200

    Update version of dependencies.

--- src/ErrorHandling/GraphQLErrorHandlerDefault.php
+++ src/ErrorHandling/GraphQLErrorHandlerDefault.php
@@ -5,6 +5,7 @@ namespace HJerichen\FrameworkGraphQL\ErrorHandling;
 use GraphQL\Error\DebugFlag;
 use GraphQL\Error\Error;
 use GraphQL\Error\FormattedError;
+use TheCodingMachine\GraphQLite\Exceptions\GraphQLException;
 
 class GraphQLErrorHandlerDefault implements GraphQLErrorHandler
 {
@@ -17,7 +18,29 @@ class GraphQLErrorHandlerDefault implements GraphQLErrorHandler
     protected function formatError(Error $error): array
     {
         $formatted = FormattedError::createFromException($error);
+        $formatted = $this->appendDebugMessage($error, $formatted);
+        $formatted = $this->appendCategory($error, $formatted);
+        return $formatted;
+    }
+
+    private function appendDebugMessage(Error $error, array $formatted): array
+    {
         $formatted = FormattedError::addDebugEntries($formatted, $error, DebugFlag::INCLUDE_DEBUG_MESSAGE);
+        if (isset($formatted['extensions']['debugMessage'])) {
+            $formatted['debugMessage'] = $formatted['extensions']['debugMessage'];
+            unset($formatted['extensions']['debugMessage']);
+        }
+        return $formatted;
+    }
+
+    private function appendCategory(Error $error, array $formatted): array
+    {
+        $exception = $error->getPrevious();
+        if ($exception instanceof GraphQLException) {
+            $formatted['extensions']['category'] = $exception->getCategory();
+        } else {
+            $formatted['extensions']['category'] = 'internal';
+        }
         return $formatted;
     }
 }
